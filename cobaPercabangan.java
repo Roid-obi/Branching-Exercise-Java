c411f804aaa4c3e7a65f61c295384fd08139faf8
Roid-obi <roidrobih@gmail.com>
2024-10-14 14:45:06 +0700

    first commit

---
+++ cobaPercabangan.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+// Pseudocode
+// Star
+// String nama
+// double nilai
+// 
+// End
+
+public class cobaPercabangan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // inisialisasi
+        String nama, keterangan, nilaiHuruf;
+        double nilai;
+
+        // Input nama mahasiswa
+        System.out.print("Masukkan nama mahasiswa: ");
+        nama = scanner.nextLine();
+
+        // Menggunakan perulangan untuk validasi nilai
+        while (true) {
+            System.out.print("Masukkan nilai mahasiswa: ");
+            nilai = scanner.nextDouble();
+
+            // Validasi nilai
+            if (nilai > 100 || nilai < 0) {
+                System.out.println("Nilai tidak valid. Silakan masukkan nilai antara 0 dan 100.");
+            } else {
+                break; // Keluar dari loop jika nilai valid
+            }
+        }
+
+        // Menggunakan ternary untuk menentukan kelulusan
+        keterangan = (nilai >= 60) ? "lulus" : "tidak lulus";
+
+         // Konversi nilai ke huruf
+         if (nilai <= 49) {
+            nilaiHuruf = "E";
+        } else if (nilai >= 50 && nilai <= 59) {
+            nilaiHuruf = "D";
+        } else if (nilai >= 60 && nilai <= 74) {
+            nilaiHuruf = "C";
+        } else if (nilai >= 75 && nilai <= 84) {
+            nilaiHuruf = "B";
+        } else {
+            nilaiHuruf = "A";
+        }
+
+        // Menampilkan hasil
+        System.out.println("Mahasiswa " + nama + " dinyatakan " + keterangan + ".");
+        System.out.println("Nilai kamu: " + nilaiHuruf);
+
+        scanner.close();
+
+    }
+}
